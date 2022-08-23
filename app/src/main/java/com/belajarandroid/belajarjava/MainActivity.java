73731d6735b3336296187171b9c002a2e7cacb0f
iqbal <iqbalvzr@gmail.com>
2022-08-23 16:21:05 +0700

    Penambahan Activity Java Dasar

--- app/src/main/java/com/belajarandroid/belajarjava/MainActivity.java
+++ app/src/main/java/com/belajarandroid/belajarjava/MainActivity.java
@@ -1,18 +1,23 @@
 package com.belajarandroid.belajarjava;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Button;
+import com.belajarandroid.belajarjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-
-
+        binding.
     }
 }
