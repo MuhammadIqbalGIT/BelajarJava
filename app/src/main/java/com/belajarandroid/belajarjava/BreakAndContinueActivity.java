38750f33d97a0871795e34e50faa9bd7774072ca
iqbal <iqbalvzr@gmail.com>
2022-08-23 14:10:23 +0700

    Penambahan Activity Java Dasar

--- app/src/main/java/com/belajarandroid/belajarjava/BreakAndContinueActivity.java
+++ app/src/main/java/com/belajarandroid/belajarjava/BreakAndContinueActivity.java
@@ -3,16 +3,12 @@ package com.belajarandroid.belajarjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class BreakAndContinueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_break_and_continue);
     }
 }
