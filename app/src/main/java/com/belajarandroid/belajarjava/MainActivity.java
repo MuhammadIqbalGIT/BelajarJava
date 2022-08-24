0e41347c9683bd488fad96f0eee4b8912eab5cdf
iqbal <iqbalvzr@gmail.com>
2022-08-24 09:19:59 +0700

    Click listener

--- app/src/main/java/com/belajarandroid/belajarjava/MainActivity.java
+++ app/src/main/java/com/belajarandroid/belajarjava/MainActivity.java
@@ -1,23 +1,36 @@
 package com.belajarandroid.belajarjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.belajarandroid.belajarjava.databinding.ActivityMainBinding;
+import com.belajarandroid.belajarjava.databinding.ActivityMethodBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-        binding.
+
+        binding.tvJavaDasar.setText("Belajar Java Dasar");
+        binding.btnHeloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HelloWorldActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
